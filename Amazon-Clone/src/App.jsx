ed203fe120848fddcaedef4ee33b1a339db49a22
abhinav12-sudo <abhitomar212007@gmail.com>
2026-03-01 00:33:28 +0530

    Setup the footer and header in the amazon clone.

--- Amazon-Clone/src/App.jsx
+++ Amazon-Clone/src/App.jsx
@@ -1,6 +1,9 @@
+import amazonLogo from './assets/d49fa3523c84bef766b4b2bde8296d23.webp';
 function App() {
   return (
     <>
+    <h1 className="bg-amber-400">Hello world</h1>
+    <img src={amazonLogo} alt="" className="h-40 w-3xl"/>
     </>
   )
 }
---
+++ Amazon-Clone/src/Layout.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+function Layout(){
+    <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </>
+}
+export default Layout
