d5e71cd5d7295aa11a5d328454f33a24b0d9d122
abhinav12-sudo <abhitomar212007@gmail.com>
2026-03-02 16:17:11 +0530

    Make HTML structure for the amazon clone.

--- Amazon-Clone/src/components/Header/Header.jsx
+++ Amazon-Clone/src/components/Header/Header.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <>
+    <div>
+      <div className="left">
+        <div className="logo"></div>
+        <div className="location"></div>
+      </div>
+      <div className="middle">
+        <div className="search">
+          <form action="">
+            <div className="form-left"></div>
+            <div className="form-middle">
+              <div>
+                <label htmlFor="search-bar"></label>
+                <input type="text" id="search-bar" />
+              </div>
+            </div>
+            <div className="form-right"></div>
+          </form>
+        </div>
+      </div>
+      <div className="right">
+        <div className="main">
+          <div className="country"></div>
+          <div className="signin"></div>
+          <div className="orders"></div>
+          <div className="cart"></div>
+        </div>
+      </div>
+
+    </div>
+    </>
   )
 }
 
