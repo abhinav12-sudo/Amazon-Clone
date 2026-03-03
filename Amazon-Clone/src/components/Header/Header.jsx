9a61a183ca52ed5303da23274ad93cea29958e0f
abhinav12-sudo <abhitomar212007@gmail.com>
2026-03-03 22:00:53 +0530

    Update the location div and add icons.

--- Amazon-Clone/src/components/Header/Header.jsx
+++ Amazon-Clone/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import amazonlogo from './int (3).png'
+import { MapPin } from 'lucide-react'
 
 function Header() {
   return (
@@ -9,7 +10,14 @@ function Header() {
         <div className="logo flex overflow-hidden items-center h-16">
           <img className=' h-20  object-cover p-2' src={amazonlogo} alt="" />
         </div>
-        <div className="location"></div>
+        <div className="location flex items-center">
+          <MapPin color='#ffff' className='w-5 h-5'/>
+          <div>
+            <p className='text-[#ccc] text-xs'>Delivering to Mumbai 400001</p>
+            <p className='text-white font-extrabold text-sm'>Update location</p>
+          </div>
+
+        </div>
       </div>
       <div className="middle">
         <div className="search">
